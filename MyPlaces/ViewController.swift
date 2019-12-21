65df3f1ea6045cbb0bbb62213620bee2acdcf47b
Kirill <36568961+sigseg5@users.noreply.github.com>
2019-12-21 14:54:44 +0500

    Added intitial project files

---
+++ MyPlaces/ViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  MyPlaces
+//
+//  Created by Kirill Parhomenko on 21.12.2019.
+//  Copyright © 2019 Kirill Parhomenko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
+extension ViewController: UITableViewDataSource, UITableViewDelegate {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell")
+        cell?.textLabel?.text = "Cell"
+        return cell!
+    }
+}
