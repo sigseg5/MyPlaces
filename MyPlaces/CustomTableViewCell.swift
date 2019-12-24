762d7083e2979bdb8feb5c3d424ee68919c782cf
Kirill <36568961+sigseg5@users.noreply.github.com>
2019-12-24 12:36:30 +0500

    Added custom class for cell, added outlets

---
+++ MyPlaces/CustomTableViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  CustomTableViewCell.swift
+//  MyPlaces
+//
+//  Created by Kirill Parhomenko on 24.12.2019.
+//  Copyright © 2019 Kirill Parhomenko. All rights reserved.
+//
+
+import UIKit
+
+class CustomTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var imageOfPlace: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var locationLabel: UILabel!
+    @IBOutlet weak var typeLabel: UILabel!
+    
+}
