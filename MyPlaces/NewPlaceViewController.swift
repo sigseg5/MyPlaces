cc6c360e3fc9fce6fab1a2754cb18843402bc853
Kirill <36568961+sigseg5@users.noreply.github.com>
2020-01-02 22:30:58 +0500

    Added empty alert controller for choosing image in new place VC

--- MyPlaces/NewPlaceViewController.swift
+++ MyPlaces/NewPlaceViewController.swift
@@ -18,6 +18,22 @@ class NewPlaceViewController: UITableViewController {
 //    MARK: Table view delegate
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         if indexPath.row == 0 {
+            let actionSheet = UIAlertController(title: nil,
+                                                message: nil,
+                                                preferredStyle: .actionSheet)
+            let cameraAction = UIAlertAction(title: "Camera", style: .default) { _ in
+                self.chooseImgPicker(source: .camera)
+                
+            }
+            let photoAction = UIAlertAction(title: "Photo", style: .default) { _ in
+                self.chooseImgPicker(source: .photoLibrary)
+            }
+            let cancelAction = UIAlertAction(title: "Cancel", style: .cancel)
+            
+            actionSheet.addAction(cameraAction)
+            actionSheet.addAction(photoAction)
+            actionSheet.addAction(cancelAction)
+            present(actionSheet, animated: true)
             
         } else {
             view.endEditing(true)
@@ -32,5 +48,17 @@ extension NewPlaceViewController: UITextFieldDelegate {
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
         textField.resignFirstResponder()
         return true
+    }
+}
+
+// MARK: work with img
+extension NewPlaceViewController {
+    func chooseImgPicker(source: UIImagePickerController.SourceType) {
+        if UIImagePickerController.isSourceTypeAvailable(source) {
+            let imagePicker = UIImagePickerController()
+            imagePicker.allowsEditing = true
+            imagePicker.sourceType = source
+            present(imagePicker, animated: true, completion: nil)
+        }
     }
 }
