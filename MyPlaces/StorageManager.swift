f11279496146f8139e5da75b435c06404d5069d7
Kirill <36568961+sigseg5@users.noreply.github.com>
2020-01-06 12:48:30 +0500

    Added Db manager for saving temp data for testing

---
+++ MyPlaces/StorageManager.swift
@@ -0,0 +1,19 @@
+//
+//  StorageManager.swift
+//  MyPlaces
+//
+//  Created by Kirill Parhomenko on 6.1.2020.
+//  Copyright © 2020 Kirill Parhomenko. All rights reserved.
+//
+
+import RealmSwift
+
+let realm = try! Realm()
+
+class StorageManager {
+    static func saveObject(_ place: Place) {
+        try! realm.write {
+            realm.add(place)
+        }
+    }
+}
