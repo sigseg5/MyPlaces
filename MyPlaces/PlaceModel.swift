91928613d5023c5396e7ca41d3825109c18ae578
Kirill <36568961+sigseg5@users.noreply.github.com>
2020-01-06 12:43:03 +0500

    Updated model for Realm

--- MyPlaces/PlaceModel.swift
+++ MyPlaces/PlaceModel.swift
@@ -6,29 +6,31 @@
 //  Copyright © 2019 Kirill Parhomenko. All rights reserved.
 //
 
-import UIKit
+import RealmSwift
 
-struct Place {
-    var name: String
-    var location: String?
-    var type: String?
-    var image: UIImage?
-    var restaurantImage: String?
+class Place: Object {
+    @objc dynamic var name: String = ""
+    @objc dynamic var location: String?
+    @objc dynamic var type: String?
+    @objc dynamic var imageData: Data?
     
-    static let restaurantNames = [
+    let restaurantNames = [
             "Burger Heroes", "Kitchen", "Bonsai", "Дастархан",
             "Индокитай", "X.O", "Балкан Гриль", "Sherlock Holmes",
             "Speak Easy", "Morris Pub", "Вкусные истории",
             "Классик", "Love&Life", "Шок", "Бочка"
     ]
 
-    static func getPLaces() -> [Place] {
-        var places = [Place]()
-        
+    func savePlacesToRealm() {
         for place in restaurantNames {
-            places.append(Place(name: place, location: "Москва", type: "Ресторан", image: nil, restaurantImage: place))
+            let image = UIImage(named: place)
+            guard let imageData = image?.pngData() else { return }
+            
+            let newPlace = Place()
+            newPlace.name = place
+            newPlace.location = "Moscow"
+            newPlace.type = "Restarant"
+            newPlace.imageData = imageData
         }
-        
-        return places
     }
 }
