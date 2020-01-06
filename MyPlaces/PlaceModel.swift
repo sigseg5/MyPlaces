e6a4fef18e30075c741c253b02d3c5c744af816a
Kirill <36568961+sigseg5@users.noreply.github.com>
2020-01-06 15:35:32 +0500

    Added convenience init to model

--- MyPlaces/PlaceModel.swift
+++ MyPlaces/PlaceModel.swift
@@ -13,4 +13,12 @@ class Place: Object {
     @objc dynamic var location: String?
     @objc dynamic var type: String?
     @objc dynamic var imageData: Data?
+    
+    convenience init(name: String, location: String?, type: String?, imageData: Data) {
+        self.init()
+        self.name = name
+        self.location = location
+        self.type = type
+        self.imageData = imageData
+    }
 }
