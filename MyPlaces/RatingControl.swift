7040e1e5965f0226d0e44dda2e57f9ef7f2775b0
Kirill <36568961+sigseg5@users.noreply.github.com>
2020-01-16 20:20:56 +0500

    Prepare for adding custom rating control

---
+++ MyPlaces/RatingControl.swift
@@ -0,0 +1,73 @@
+//
+//  RatingControl.swift
+//  MyPlaces
+//
+//  Created by Kirill Parhomenko on 16.1.2020.
+//  Copyright © 2020 Kirill Parhomenko. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable class RatingControl: UIStackView {
+    
+    //MARK: Props
+    private var ratingButtons = [UIButton]()
+    
+    @IBInspectable var starSize: CGSize = CGSize(width: 44.0, height: 44.0) {
+        didSet {
+            setupButtons()
+        }
+    }
+    @IBInspectable var starCount: Int = 5 {
+        didSet {
+            setupButtons()
+        }
+    }
+    
+    var rating = 0
+    
+    //MARK: Initialization
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupButtons()
+    }
+    
+    required init(coder: NSCoder) {
+        super.init(coder: coder)
+        setupButtons()
+    }
+    
+    //MARK: Button action
+    @objc func retignButtonTapped(button: UIButton) {
+        print("Button pressed!")
+    }
+    
+    //MARK: Private Methods
+    private func setupButtons() {
+        for button in ratingButtons {
+            removeArrangedSubview(button)
+            button.removeFromSuperview()
+        }
+        ratingButtons.removeAll()
+        
+        for _ in 0..<starCount {
+            //Create button
+            let button = UIButton()
+            button.backgroundColor = .red
+            
+            //Add constraints
+            button.translatesAutoresizingMaskIntoConstraints = false
+            button.heightAnchor.constraint(equalToConstant: starSize.height).isActive = true
+            button.widthAnchor.constraint(equalToConstant: starSize.width).isActive = true
+            
+            //Setup the button action
+            button.addTarget(self, action: #selector(retignButtonTapped(button:)), for: .touchUpInside)
+            
+            //Add button to the stack view
+            addArrangedSubview(button)
+            
+            ratingButtons.append(button)
+        }
+    }
+
+}
