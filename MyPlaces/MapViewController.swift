83baca673b1e253995ae0f1f2fa2b7684dc9074a
Kirill <36568961+sigseg5@users.noreply.github.com>
2020-01-25 22:26:12 +0500

    Added mapVC

---
+++ MyPlaces/MapViewController.swift
@@ -0,0 +1,20 @@
+//
+//  MapViewController.swift
+//  MyPlaces
+//
+//  Created by Kirill Parhomenko on 25.1.2020.
+//  Copyright © 2020 Kirill Parhomenko. All rights reserved.
+//
+
+import UIKit
+
+class MapViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func cancelMapVC() {
+        dismiss(animated: true, completion: nil)
+    }
+}
