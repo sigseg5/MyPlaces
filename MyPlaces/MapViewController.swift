31ae88f5537a3e2f40721b9409adef3c9414fdad
Kirill <36568961+sigseg5@users.noreply.github.com>
2020-01-29 19:36:30 +0500

    Added map annotation

--- MyPlaces/MapViewController.swift
+++ MyPlaces/MapViewController.swift
@@ -11,13 +11,37 @@ import MapKit
 
 class MapViewController: UIViewController {
   @IBOutlet weak var mapView: MKMapView!
-  var place: Place! 
+  var place: Place!
   
   override func viewDidLoad() {
     super.viewDidLoad()
+    setupPlacemark()
   }
   
   @IBAction func cancelMapVC() {
     dismiss(animated: true, completion: nil)
+  }
+  
+  private func setupPlacemark() {
+    guard let location = place.location else { return }
+    let geocoder = CLGeocoder()
+    geocoder.geocodeAddressString(location) { (placemarksArray, error) in
+      
+      if let error = error {
+        print(error)
+        return
+      }
+      
+      guard let placemarksArray = placemarksArray else { return }
+      let placemark = placemarksArray.first
+      let annotation = MKPointAnnotation()
+      annotation.title = self.place.name
+      annotation.subtitle = self.place.type
+      
+      guard let placemarkLocation = placemark?.location else { return }
+      annotation.coordinate = placemarkLocation.coordinate
+      self.mapView.showAnnotations([annotation], animated: true)
+      self.mapView.selectAnnotation(annotation, animated: true)
+    }
   }
 }
